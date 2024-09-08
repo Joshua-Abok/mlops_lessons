04182f19f54fcde3c09896da9931c25d2aa6cee3
Joshua Abok <nyajuaya.j.a@gmail.com>
2024-09-08 20:39:24 +0300

    tkinter option 1 and 2 working

--- c950/main.py
+++ c950/main.py
@@ -1,36 +1,93 @@
 from load_trucks import *
 from options import *
 
-def main():
-    print("Welcome to WGUPS!!")
-    print("1. Print Total Mileage")
-    print("2. Get All Packages at a single time.")
-    print("3. Get A Specific package at a single time.")
-    print("4. Exit the Program")
+# def main():
+#     print("Welcome to WGUPS!!")
+#     print("1. Print Total Mileage")
+#     print("2. Get All Packages at a single time.")
+#     print("3. Get A Specific package at a single time.")
+#     print("4. Exit the Program")
 
-    #Continuous loop to display menu, breaks once user enters 4.
-    #O(n^2) due to case 2 and case 3 containing O(n) functions.
-    while True:
-        try:
-            i = int(input("Please enter one of the following options [1,2,3,4]: "))
-            match i:
-                case 1:    # precautionary measure
-                    print(f'Total distance traveled is {lt.get_total_distance():.2f} miles.\n')
-                case 2:
-                    time = input('Enter time in (HH:MM:SS) format: ')
-                    get_all_packages(lt.hm, time)
-                    break
-                case 3:
-                    id = int(input('Enter package ID: '))
-                    time = input('Enter time in (HH:MM:SS) format: ')
-                    get_single_package(lt.hm, id,time)
-                    break
-                case 4:
-                    print('Have a good day.')
-                    break
+#     #Continuous loop to display menu, breaks once user enters 4.
+#     #O(n^2) due to case 2 and case 3 containing O(n) functions.
+#     while True:
+#         try:
+#             i = int(input("Please enter one of the following options [1,2,3,4]: "))
+#             match i:
+#                 case 1:    # precautionary measure
+#                     print(f'Total distance traveled is {lt.get_total_distance():.2f} miles.\n')
+#                 case 2:
+#                     time = input('Enter time in (HH:MM:SS) format: ')
+#                     # get_all_packages(lt.hm, time)
+#                     options.get_all_packages(time)
+#                     break
+#                 case 3:
+#                     id = int(input('Enter package ID: '))
+#                     time = input('Enter time in (HH:MM:SS) format: ')
+#                     # get_single_package(lt.hm, id,time)
+#                     options.get_single_package(id,time)
+#                     break
+#                 case 4:
+#                     print('Have a good day.')
+#                     break
 
-        except:
-            print('Invalid input, try again [main file].')
+#         except:
+#             print('Invalid input, try again [main file].')
                 
+# if __name__ == "__main__":
+#     main()
+
+
+#################################################################################################
+
+
+#################################################################################################
+
+import tkinter as tk
+from tkinter import messagebox
+import tkinter.simpledialog
+# from load_trucks import get_total_distance
+# from options import get_all_packages, get_single_package
+
+class WGUPSApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("WGUPS Delivery System")
+        
+        # Welcome Label
+        self.welcome_label = tk.Label(root, text="Welcome to WGUPS!!", font=("Helvetica", 16))
+        self.welcome_label.pack(pady=10)
+        
+        # Option Buttons
+        self.btn_total_mileage = tk.Button(root, text="1. Print Total Mileage", command=self.show_total_mileage)
+        self.btn_total_mileage.pack(pady=5)
+
+        self.btn_all_packages = tk.Button(root, text="2. Get All Packages at a Single Time", command=self.get_all_packages)
+        self.btn_all_packages.pack(pady=5)
+
+        self.btn_single_package = tk.Button(root, text="3. Get A Specific Package at a Single Time", command=self.get_single_package)
+        self.btn_single_package.pack(pady=5)
+
+        self.btn_exit = tk.Button(root, text="4. Exit the Program", command=root.quit)
+        self.btn_exit.pack(pady=5)
+        
+    def show_total_mileage(self):
+        total_distance = lt.get_total_distance()
+        messagebox.showinfo("Total Mileage", f"Total distance traveled is {total_distance:.2f} miles.")
+    
+    def get_all_packages(self):
+        time = tk.simpledialog.askstring("Input", "Enter time in (HH:MM:SS) format:")
+        if time:
+            options.get_all_packages(time)  # This should print or process packages
+    
+    def get_single_package(self):
+        package_id = tk.simpledialog.askinteger("Input", "Enter package ID:")
+        time = tk.simpledialog.askstring("Input", "Enter time in (HH:MM:SS) format:")
+        if package_id and time:
+            options.get_single_package(package_id, time)  # This should print or process the specific package
+
+# Run the application
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = WGUPSApp(root)
+    root.mainloop()
--- c950/options.py
+++ c950/options.py
@@ -2,69 +2,156 @@ import datetime
 
 #Return information for all of the packages that match a given time
 #O(n)
-def get_all_packages(hm, time):
-    try:
-        (h, m, s) = time.split(':')
-        time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-        for i in range(1, 41):
+# def get_all_packages(hm, time):
+#     try:
+#         (h, m, s) = time.split(':')
+#         time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+#         for i in range(1, 41):
             
-            try:
-                start = hm.search(i).start
-                status = hm.search(i).status
-                (h,m,s) = start[0].split(':')
-                start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-                (h,m,s) = status.split(':')
-                status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-            except ValueError:
-                pass
+#             try:
+#                 start = hm.search(i).start
+#                 status = hm.search(i).status
+#                 (h,m,s) = start[0].split(':')
+#                 start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+#                 (h,m,s) = status.split(':')
+#                 status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+#             except ValueError:
+#                 pass
             
-            if start >= time:
-                hm.search(i).status = 'At Hub'
-                hm.search(i).start = 'Departing at ' + str(start)
-                print(hm.search(i).__str__()) 
+#             if start >= time:
+#                 hm.search(i).status = 'At Hub'
+#                 hm.search(i).start = 'Departing at ' + str(start)
+#                 print(hm.search(i).__str__()) 
 
-            elif start <= time:
-                if status > time:
-                    hm.search(i).status = 'In Route'
-                    hm.search(i).start = 'Departed at ' + str(start)
-                    print(hm.search(i).__str__())  
-                else:
-                    hm.search(i).status = 'Delivered at ' + str(status)
-                    hm.search(i).start = 'Departed at ' + str(start)
-                    print(hm.search(i).__str__()) 
+#             elif start <= time:
+#                 if status > time:
+#                     hm.search(i).status = 'In Route'
+#                     hm.search(i).start = 'Departed at ' + str(start)
+#                     print(hm.search(i).__str__())  
+#                 else:
+#                     hm.search(i).status = 'Delivered at ' + str(status)
+#                     hm.search(i).start = 'Departed at ' + str(start)
+#                     print(hm.search(i).__str__()) 
                     
 
-    except:
-        print('Invalid input, try again.')
+#     except:
+#         print('Invalid input, try again.')
 
-#Return a single package that matches a given id and time
-#O(1)
-def get_single_package(hm, id, time):
-    try:
-        start = hm.search(id).start
-        status = hm.search(id).status
-        (h,m,s) = start[0].split(':')
-        start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-        (h,m,s) = status.split(':')
-        status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-        (h,m,s) = time.split(':')
-        time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+# #Return a single package that matches a given id and time
+# #O(1)
+# def get_single_package(hm, id, time):
+#     try:
+#         start = hm.search(id).start
+#         status = hm.search(id).status
+#         (h,m,s) = start[0].split(':')
+#         start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+#         (h,m,s) = status.split(':')
+#         status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+#         (h,m,s) = time.split(':')
+#         time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
 
-        if start >= time:
-            hm.search(id).status = 'At Hub'
-            hm.search(id).start = 'Departing at ' + str(start)
-            print('Package ID: ' + str(hm.search(id).ID))
-            print('Status of Delivery: ' + str(hm.search(id).status))
+#         if start >= time:
+#             hm.search(id).status = 'At Hub'
+#             hm.search(id).start = 'Departing at ' + str(start)
+#             print('Package ID: ' + str(hm.search(id).ID))
+#             print('Status of Delivery: ' + str(hm.search(id).status))
 
-        elif start <= time:
-            if time < status:
-                hm.search(id).status = 'In Route'
-                hm.search(id).start = 'Departed at ' + str(start)
-                print(hm.search(id).__str__())  
-            else:
-                hm.search(id).status = 'Delivered at ' + str(status)
-                hm.search(id).start = 'Departed at ' + str(start)
-                print(hm.search(id).__str__())  
+#         elif start <= time:
+#             if time < status:
+#                 hm.search(id).status = 'In Route'
+#                 hm.search(id).start = 'Departed at ' + str(start)
+#                 print(hm.search(id).__str__())  
+#             else:
+#                 hm.search(id).status = 'Delivered at ' + str(status)
+#                 hm.search(id).start = 'Departed at ' + str(start)
+#                 print(hm.search(id).__str__())  
 
-    except:
-        print('Invalid input, try again.')
+#     except:
+#         print('Invalid input, try again.')
+
+
+
+####################################################################################
+
+
+####################################################################################
+
+import datetime
+from load_trucks import *
+
+class PackageHandler:
+    def __init__(self, hashtable):
+        self.hm = hashtable
+
+    # Return information for all of the packages that match a given time
+    # O(n)
+    def get_all_packages(self, time):
+        try:
+            (h, m, s) = time.split(':')
+            time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+            for i in range(1, 41):  # Assuming there are 40 packages
+                
+                try:
+                    package = self.hm.search(i)
+                    start = package.start
+                    status = package.status
+                    (h, m, s) = start[0].split(':')
+                    start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+                    (h, m, s) = status.split(':')
+                    status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+                except ValueError:
+                    pass
+
+                if start >= time:
+                    package.status = 'At Hub'
+                    package.start = 'Departing at ' + str(start)
+                    print(package)
+
+                elif start <= time:
+                    if status > time:
+                        package.status = 'In Route'
+                        package.start = 'Departed at ' + str(start)
+                        print(package)
+                    else:
+                        package.status = 'Delivered at ' + str(status)
+                        package.start = 'Departed at ' + str(start)
+                        print(package)
+
+        except:
+            print('Invalid input, try again.')
+
+    # Return a single package that matches a given id and time
+    # O(1)
+    def get_single_package(self, id, time):
+        try:
+            package = self.hm.search(id)
+            start = package.start
+            status = package.status
+            (h, m, s) = start[0].split(':')
+            start = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+            (h, m, s) = status.split(':')
+            status = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+            (h, m, s) = time.split(':')
+            time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+
+            if start >= time:
+                package.status = 'At Hub'
+                package.start = 'Departing at ' + str(start)
+                print('Package ID:', package.ID)
+                print('Status of Delivery:', package.status)
+
+            elif start <= time:
+                if time < status:
+                    package.status = 'In Route'
+                    package.start = 'Departed at ' + str(start)
+                    print(package)
+                else:
+                    package.status = 'Delivered at ' + str(status)
+                    package.start = 'Departed at ' + str(start)
+                    print(package)
+
+        except:
+            print('Invalid input, try again.')
+
+
+options = PackageHandler(lt.hm)
