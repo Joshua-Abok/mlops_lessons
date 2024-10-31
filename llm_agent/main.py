75ecfd455313e6bddb6624e799b044326e648fe2
Joshua Abok <nyajuaya.j.a@gmail.com>
2024-10-31 14:53:44 +0300

    DuckDuckGoSearchResult

--- llm_agent/main.py
+++ llm_agent/main.py
@@ -1,6 +1,24 @@
 from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools import DuckDuckGoSearchResults
+import re 
 
-search = DuckDuckGoSearchRun()
+# search = DuckDuckGoSearchRun()
 
-output = search.invoke('joshua abok')
-print(output)
+# output = search.invoke('joshua abok')
+# print(output)
+
+search = DuckDuckGoSearchResults()
+output = search.run("Joshua abok")
+# print(output)
+
+#  user friendly output
+# -> use regular exp. to etract the snippet, link & the title 
+# define a pattern 
+pattern = r'snippet: (.*?), title: (.*?), link: (.*?)\],'
+
+# call findall function to find all matches of the pattern in the string 
+matches = re.findall(pattern, output, re.DOTALL)
+
+for snippet, title, link in matches: 
+    print(f'Snippet: {snippet}\nTitle: {title}\nLink: {link}\n')
+    print('-' * 50)
